3d6f40cd328c5c658e992cf72805864ac82eac92
Yevhenii Zotkin <yevhenii.zotkin@protonmail.com>
2020-11-13 15:54:23 +0100

    change forward to accept embedding in concatenated format

--- inclearn/lib/losses/nt_xent.py
+++ inclearn/lib/losses/nt_xent.py
@@ -5,12 +5,12 @@ import torch.nn as nn
 
 
 class NT_Xent(nn.Module):
-    def __init__(self, batch_size, temperature):
+    def __init__(self, batch_size, temperature, device):
         super(NT_Xent, self).__init__()
         self.batch_size = batch_size
         self.temperature = temperature
         # TODO this might potentially lead to problems
-        self.device = torch.device("cuda")
+        self.device = device
         self.criterion = nn.CrossEntropyLoss(reduction="sum")
         self.similarity_f = nn.CosineSimilarity(dim=2)
 
@@ -22,32 +22,29 @@ class NT_Xent(nn.Module):
             mask[batch_size + i, i] = 0
         return mask
 
-    def forward(self, z_i, z_j):
+    def forward(self, p1):
         """
         We do not sample negative examples explicitly.
         Instead, given a positive pair, similar to (Chen et al., 2017), we treat the other 2(N − 1) augmented examples within a minibatch as negative examples.
         """
-        batch_size = z_i.shape[0]
-        mask = self.get_correlated_samples_mask(batch_size)
-
-        p1 = torch.cat((z_i, z_j), dim=0)
+        mask = self.get_correlated_samples_mask(self.batch_size)
 
         sim = self.similarity_f(p1.unsqueeze(1), p1.unsqueeze(0)) / self.temperature
 
-        sim_i_j = torch.diag(sim, batch_size)
-        sim_j_i = torch.diag(sim, -batch_size)
+        sim_i_j = torch.diag(sim, self.batch_size)
+        sim_j_i = torch.diag(sim, -self.batch_size)
 
         positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(
-            batch_size * 2, 1
+            self.batch_size * 2, 1
         )
-        negative_samples = sim[mask].reshape(batch_size * 2, -1)
+        negative_samples = sim[mask].reshape(self.batch_size * 2, -1)
 
         logits = torch.cat((positive_samples, negative_samples), dim=1)
         # TODO According to documentation https://pytorch-lightning.readthedocs.io/en/latest/multi_gpu.html,
         # if you inherit from LightningModule your class should have attribute device
         # In practice, such attribute is absent
-        labels = torch.zeros(batch_size * 2).to(self.device).long()
+        labels = torch.zeros(self.batch_size * 2).to(self.device).long()
         loss = self.criterion(logits, labels)
-        loss /= 2 * batch_size
+        loss /= 2 * self.batch_size
 
         return loss
