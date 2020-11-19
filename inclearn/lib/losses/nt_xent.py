fixed the batch size computation

inclearn/lib/losses/nt_xent.py
@@ -27,24 +27,28 @@ class NT_Xent(nn.Module):
         We do not sample negative examples explicitly.
         Instead, given a positive pair, similar to (Chen et al., 2017), we treat the other 2(N − 1) augmented examples within a minibatch as negative examples.
         """
-        mask = self.get_correlated_samples_mask(self.batch_size)
-
+        batch_size = int(p1.shape[0]/2)
+        print(f"p1 shape {p1.shape}")
+        mask = self.get_correlated_samples_mask(batch_size)
+        print(f"Batch size {batch_size}")
+        print(f"Mask shape {mask.shape}")
         sim = self.similarity_f(p1.unsqueeze(1), p1.unsqueeze(0)) / self.temperature
-
-        sim_i_j = torch.diag(sim, self.batch_size)
-        sim_j_i = torch.diag(sim, -self.batch_size)
-
+        print(f"Sim shape {sim.shape}")
+        sim_i_j = torch.diag(sim, batch_size)
+        sim_j_i = torch.diag(sim, -batch_size)
+        print(f"Sim i_j shape {sim_i_j.shape}")
+        print(f"Sim j_i shape {sim_j_i.shape}")
         positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(
-            self.batch_size * 2, 1
+            batch_size * 2, 1
         )
-        negative_samples = sim[mask].reshape(self.batch_size * 2, -1)
+        negative_samples = sim[mask].reshape(batch_size * 2, -1)
 
         logits = torch.cat((positive_samples, negative_samples), dim=1)
         # TODO According to documentation https://pytorch-lightning.readthedocs.io/en/latest/multi_gpu.html,
         # if you inherit from LightningModule your class should have attribute device
         # In practice, such attribute is absent
-        labels = torch.zeros(self.batch_size * 2).to(self.device).long()
+        labels = torch.zeros(batch_size * 2).to(self.device).long()
         loss = self.criterion(logits, labels)
-        loss /= 2 * self.batch_size
+        loss /= 2 * batch_size
 
         return loss